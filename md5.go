b9f78965449847bb1f04dd522f489392afbf1966
gibsonli <gibsonli@tencent.com>
2020-10-26 16:57:43 +0800

    add md5file

--- md5.go
+++ md5.go
@@ -3,8 +3,27 @@ package crypto
 import (
 	"crypto/md5"
 	"fmt"
+	"io"
+	"os"
 )
 
 func Md5String(data []byte) string {
 	return fmt.Sprintf("%x", md5.Sum(data))
+}
+
+func Md5File(path string) (string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+
+	h := md5.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
--- md5_test.go
+++ md5_test.go
@@ -7,8 +7,17 @@ import (
 func TestMd5String(t *testing.T) {
 	md5 := Md5String([]byte("abc"))
 	if len(md5) != 32 {
-		t.Error(string(md5))
+		t.Error(md5)
 	}
 
-	t.Log(string(md5))
+	t.Log(md5)
+}
+
+func TestMd5File(t *testing.T) {
+	md5, err := Md5File("/home/ligang/devspace/goinbox/crypto/md5.go")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(md5, err)
 }
